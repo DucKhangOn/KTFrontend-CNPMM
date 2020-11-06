8729d3a764dfc65888fcb4e9680cc2cdf19e8352
Khang On <javaneverdie0612@gmail.com>
2020-11-07 00:15:02 +0700

    add favicon

--- app/controllers/charge/charge-into-ktb.js
+++ app/controllers/charge/charge-into-ktb.js
@@ -4,8 +4,11 @@ import { action } from '@ember/object';
 export default class ChargeChargeIntoKtbController extends Controller {
     @action
     charge() {
-        // fetch('http://localhost:5000/api/paypal')
-        //     .then(response => response.json())
-        //     .then(data => console.log(data));
+        fetch('http://localhost:5000/api/paypal')
+            .then(response => response.json())
+            .then((data) => {
+                console.log(data.link);
+                window.location.href = this.link;
+            });
     }
 }
